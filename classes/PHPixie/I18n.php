45c3ad5127626fa0105b5822e1db1d51dff1bbf3
924 <924@192.168.0.100>
2014-07-03 22:24:10 +0600

    first

---
+++ classes/PHPixie/I18n.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PHPixie;
+
+/**
+ * Module for using internationalization
+ *
+ * @author Nxeed
+ * @link https://github.com/nxeed/PHPixie-Nxeed-I18n Download this module from Github
+ */
+class I18n {
+
+    /**
+     * Pixie Dependency Container
+     * 
+     * @var \PHPixie\Pixie
+     */
+    protected $pixie;
+
+    /**
+     * Initializes the I18n module
+     * 
+     * @param \PHPixie\Pixie $pixie Pixie dependency container
+     */
+    public function __construct($pixie) {
+        $this->pixie = $pixie;
+    }
+
+}
